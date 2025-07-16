5a792e5b5e3193122c122c367cbaced3aa2c2580
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-07-16 19:59:20 +0600

    Create basic_1.cpp

---
+++ Learning C++/C++ Basics/basic_1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // cout object, << operator
+    // print text
+    cout << "Hello World!" << endl;
+    
+    //print numbers 
+    cout << 3+3 << endl;
+    
+    // new line
+    cout << "new line" << '\n';
+    
+    //\t \\ \"
+    cout << "hello\t";
+    cout << "ME" << endl;
+    cout << "Hello \\";
+    cout << "C++" << endl;
+    cout << "Hello \"Jony\"" << endl;
+    /*
+    multiline 
+    comment
+    */
+    
+    // Data Types
+    int myNum = 5;             
+    double myFloatNum = 5.99;    
+    char myLetter = 'D';         
+    string myText = "Hello";
+    bool myBoolean = true;       
+    return 0;
+}
